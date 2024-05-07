ab5ae21a1152bec519afc1575079bd8771acf1d8
Gangbros777 <161280210+Gangbros777@users.noreply.github.com>
2024-05-07 09:36:30 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let userName = prompt("String typedagi malumotni kiriting")
+let userSurname = + prompt("Number typedagi malumotni kiriting")
+let userLesson = prompt("Booleen typedagi malumotni kiriting")
+let message = alert("Hamma malumotni to'g'ri kiritganizga ishonchingiz kolimmi?")
+let javob = alert(`Siz kiritgan:${userName} so'zi string typega tegishli
+Siz kiritgan:${userSurname} so'zi number typedagi tegishli
+Siz kiritgan:${userLesson} so'zi booleen typega tegishli `)
+console.log(`Siz kiritgan:${userName} so'zi string typega tegishli
+Siz kiritgan:${userSurname} so'zi number typedagi tegishli
+Siz kiritgan:${userLesson} so'zi booleen typega tegishli`);
+String
